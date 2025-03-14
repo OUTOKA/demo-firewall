7314685b2efc7d5b38a0cae2bb8bbb2c7e4b4b50
didieant <didieant@A306-04.rt3>
2025-03-14 11:14:49 +0100

    résolution pb

--- sae_Firewall/client/script/test_connectivité.sh
+++ sae_Firewall/client/script/test_connectivité.sh
@@ -1,86 +1,129 @@
 #!/bin/bash
-
+# adresses IP des différentes machines à tester
 server_dmz="192.168.58.10"
 server="192.168.57.10"
 client="192.168.57.20"
 
-#nmap -oG test57.txt 192.168.57.0/24
-#nmap -oG test58.txt 192.168.58.0/24
+#scan avec NMAP avec renvoi greppable dans des fichiers temporaires.
+nmap -oG test57.txt 192.168.57.0/24
+nmap -oG test58.txt 192.168.58.0/24
 
-echo "#######################################" > "scan-result.csv"
-echo "Test ports ouverts pour chaque machines" >> "scan-result.csv"
+echo -e "\e[31m
+
+
+#######################################
+#######################################
+#######################################\e[0m" >> "scan-result.csv"
+echo -e "\e[34mTest du $(date)\e[0m" >> "scan-result.csv"
+
+echo "
+#######################################" >> "scan-result.csv"
+echo -e "\e[34mTest ports ouverts\e[0m" >> "scan-result.csv"
 echo "#######################################" >> "scan-result.csv"
 
+# Test ports ouverts pour le server privé.
 for ip in $( cat test57.txt | grep "$server" | awk '{print $2}' | uniq); do
   open_ports_tcp=$(cat test57.txt | grep $ip | grep -oP '\d+\/open\/tcp' )
   
-  echo "Ports ouverts pour $ip:
+  echo -e "\e[34m
+Ports ouverts pour $ip:\e[0m
 $open_ports_tcp" >> "scan-result.csv"
-  echo "#######################################" >> "scan-result.csv"
+  echo "
+#######################################" >> "scan-result.csv"
 done
 
+# Test ports ouverts pour le client.
 for ip in $( cat test57.txt | grep "$client" | awk '{print $2}' | uniq); do
   open_ports_tcp=$(cat test57.txt | grep $ip | grep -oP '\d+\/open\/tcp' )
   
-  echo "Ports ouverts pour $ip:
+  echo -e "\e[34m
+Ports ouverts pour $ip:\e[0m
 $open_ports_tcp" >> "scan-result.csv"
-  echo "#######################################" >> "scan-result.csv"
+  echo "
+#######################################" >> "scan-result.csv"
 done
 
+# Test ports ouverts pour le server public.
 for ip in $( cat test58.txt | grep "$server_dmz" | awk '{print $2}' | uniq); do
   open_ports_tcp=$(cat test58.txt | grep $ip | grep -oP '\d+\/open\/tcp')
   
-  echo "Ports ouverts pour $ip: 
+  echo -e "\e[34m
+Ports ouverts pour $ip:\e[0m 
 $open_ports_tcp" >> "scan-result.csv"
-  echo "#######################################" >> "scan-result.csv"
+  echo "
+#######################################" >> "scan-result.csv"
 done
 
-echo $(cat "scan-result.csv") > "scan-result_final.csv"
+echo -e "\e[34mTests individuels.\e[0m" >> "scan-result.csv"
+echo "#######################################
+" >> "scan-result.csv"
+
 
 # Vérifier que les ports 80 et 443 sont ouverts
+echo -e "\e[34mVérifier que les ports 80 et 443 sont ouverts sur le sever web.
+\e[0m" >> "scan-result.csv"
 if cat "test58.txt" | grep "$server_dmz" | grep -q "80/open"; then
-    echo "Test réussi: Port 80 ouvert sur le serveur web." >> "scan-result_final.csv"
+    echo "Test réussi: Port 80 ouvert sur le serveur web." >> "scan-result.csv"
 else
-    echo "Test échoué: Port 80 fermé sur le serveur web." >> "scan-result_final.csv"
+    echo "Test échoué: Port 80 fermé sur le serveur web." >> "scan-result.csv"
 fi
-echo "#######################################" >> "scan-result_final.csv"
+echo "" >> "scan-result.csv"
 if cat "test58.txt" | grep "$server_dmz" | grep -q "443/open"; then
-    echo "Test réussi: Port 443 ouvert sur le serveur web." >> "scan-result_final.csv"
+    echo "Test réussi: Port 443 ouvert sur le serveur web." >> "scan-result.csv"
 else
-    echo "Test échoué: Port 443 fermé sur le serveur web." >> "scan-result_final.csv"
+    echo "Test échoué: Port 443 fermé sur le serveur web." >> "scan-result.csv"
 fi
-echo "#######################################" >> "scan-result_final.csv"
+echo "
+#######################################
+" >> "scan-result.csv"
 # Tester la réponse HTTP du serveur web avec curl
-echo "Test de la réponse HTTP du serveur web avec curl..." >> "scan-result_final.csv"
+echo -e "\e[34mTest de la réponse HTTP du serveur web avec curl
+\e[0m" >> "scan-result.csv"
 if curl -s --head http://$server_dmz/ | grep -q "200 OK"; then
-    echo "Test réussi: Réponse HTTP 200 OK sur le port 80." >> "scan-result_final.csv"
+    echo "Test réussi: Réponse HTTP OK sur le port 80." >> "scan-result.csv"
 else
-    echo "Test échoué: Pas de réponse HTTP 200 OK sur le port 80." >> "scan-result_final.csv"
+    echo "Test échoué: Pas de réponse HTTP OK sur le port 80." >> "scan-result.csv"
 fi
-echo "#######################################" >> "scan-result_final.csv"
-# Tester la connectivité du client vers le serveur dans le réseau privé avec nmap
-echo "Test de la réponse HTTP du serveur web privé avec curl..." >> "scan-result_final.csv"
+echo "
+#######################################
+" >> "scan-result.csv"
+# Tester la connectivité du client vers le serveur dans le réseau privé avec curl
+echo -e "\e[34mTest de la réponse HTTP du serveur web privé avec curl
+\e[0m" >> "scan-result.csv"
 if curl -s --head http://$server/ | grep -q "200 OK"; then
-    echo "Test réussi: Réponse HTTP 200 OK sur le port 80." >> "scan-result_final.csv"
+    echo "Test réussi: Réponse HTTP OK sur le port 80." >> "scan-result.csv"
 else
-    echo "Test échoué: Pas de réponse HTTP 200 OK sur le port 80." >> "scan-result_final.csv"
+    echo "Test échoué: Pas de réponse HTTP OK sur le port 80." >> "scan-result.csv"
 fi
-echo "#######################################" >> "scan-result_final.csv"
+echo "
+#######################################
+" >> "scan-result.csv"
 # Vérifier que le port 22 (SSH) est accessible depuis le client
+echo -e "\e[34mVérifier que le port 22 (SSH) est accessible depuis le client sur le server privé
+\e[0m" >> "scan-result.csv"
 if cat "test57.txt" | grep "$sever" | grep -q "22/open"; then
-    echo "Test réussi: Port 22 ouvert sur le serveur privé." >> "scan-result_final.csv"
+    echo "Test réussi: Port 22 ouvert sur le serveur privé." >> "scan-result.csv"
 else
-    echo "Test échoué: Port 22 fermé sur le serveur privé." >> "scan-result_final.csv"
+    echo "Test échoué: Port 22 fermé sur le serveur privé." >> "scan-result.csv"
 fi
-echo "#######################################" >> "scan-result_final.csv"
+echo "
+#######################################
+" >> "scan-result.csv"
 
 # Vérifier que le port 22 (SSH) est accessible depuis le réseau privé vers la DMZ
-if cat "test57.txt"| grep "$server" | grep -q "22/open"; then
-    echo "Test réussi: Port 22 ouvert sur le serveur web dans la DMZ." >> "scan-result_final.csv"
+# Écrire le texte en bleu dans le fichier scan-result.csv
+echo -e "\e[34mVérifier que le port 22 (SSH) est accessible depuis le client sur le serveur public\e[0m" >> scan-result.csv
+
+if cat "test58.txt"| grep "$server_dmz" | grep -q "22/open"; then
+    echo "Test réussi: Port 22 ouvert sur le serveur web dans la DMZ." >> "scan-result.csv"
 else
-    echo "Test échoué: Port 22 fermé sur le serveur web dans la DMZ." >> "scan-result_final.csv"
+    echo "Test échoué: Port 22 fermé sur le serveur web dans la DMZ." >> "scan-result.csv"
 fi
-echo "#######################################" >> "scan-result_final.csv"
-echo "Tests terminés."
-echo "#######################################" >> "scan-result_final.csv"
-cat "scan-result_final.csv"
+echo "
+#######################################" >> "scan-result.csv"
+echo -e "\e[34mTests terminés.\e[0m" >> "scan-result.csv" 
+echo "#######################################" >> "scan-result.csv"
+cat "scan-result.csv"
+
+rm "test57.txt"
+rm "test58.txt"
--- sae_Firewall/server/script/script_ufw.sh
+++ sae_Firewall/server/script/script_ufw.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
+# Réinitialiser toutes les règles ufw existantes
 yes y | ufw reset
+
+# Autoriser le trafic SSH (port 22) depuis l'adresse IP 192.168.56.1 sur le sever
 ufw allow from 192.168.56.1 to any port 22
+
+# Autoriser tout le trafic provenant du sous-réseau 192.168.57.0/24 vers n'importe quelle destination
 ufw allow from 192.168.57.0/24 to any
+
+# Autoriser le trafic HTTP (port 80) vers le serveur web situé à l'adresse 192.168.58.10 (DMZ)
 ufw allow proto tcp from any to 192.168.58.10 port 80
+
+# Autoriser le trafic HTTPS (port 443) vers le serveur web situé à l'adresse 192.168.58.10
 ufw allow proto tcp from any to 192.168.58.10 port 443
+
+# Autoriser le trafic du sous-réseau privé 192.168.57.0/24 vers le sous-réseau DMZ 192.168.58.0/24
 ufw allow from 192.168.57.0/24 to 192.168.58.0/24
+
+# Autoriser le trafic du client 192.168.57.20 vers le serveur 192.168.57.10 dans le réseau privé
 ufw allow from 192.168.57.20 to 192.168.57.10
+
+# Définir les rêgles par défaut pour bloquer tout le trafic entrant non autorisé
 ufw default deny incoming
+
+# Définir les rêgles par défaut pour autoriser tout le trafic sortant
 ufw default allow outgoing
+
+# Activer ufw
 yes y | ufw enable
+
+# Afficher le status en détails des règles ufw
 ufw status verbose
 
